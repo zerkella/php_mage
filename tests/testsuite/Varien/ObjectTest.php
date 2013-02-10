ff066dd490c2278d5f5493caff3d413e05ee24a3
Andrey Tserkus <andrey.tserkus@magento.com>
2013-02-09 22:21:38 -0800

    Implemented all properties - added easy way to setup properties - added all the properties from version 1.7.0.2

--- tests/testsuite/Varien/ObjectTest.php
+++ tests/testsuite/Varien/ObjectTest.php
@@ -17,9 +17,9 @@ class Varien_ObjectTest extends PHPUnit_Framework_TestCase
      * @param string $property
      * @param array $modifiers
      * @param string $defaultValue
-     * @dataProvider internalPropertiesDataProvider
+     * @dataProvider propertiesDataProvider
      */
-    public function testInternalProperties($property, $modifiers, $defaultValue)
+    public function testProperties($property, $modifiers, $defaultValue)
     {
         $this->assertClassHasAttribute($property, 'Varien_Object');
 
@@ -33,10 +33,39 @@ class Varien_ObjectTest extends PHPUnit_Framework_TestCase
         $this->assertAttributeSame($defaultValue, $property, $obj, "Property {$property} has wrong default value");
     }
 
-    public static function internalPropertiesDataProvider()
+    public static function propertiesDataProvider()
     {
         return array(
             array('_data', array('protected'), array()),
+            array('_hasDataChanges', array('protected'), false),
+            array('_origData', array('protected'), null),
+            array('_idFieldName', array('protected'), null),
+            array('_underscoreCache', array('protected'), array()),
+            array('_isDeleted', array('protected'), false),
+            array('_oldFieldsMap', array('protected'), array()),
+            array('_syncFieldsMap', array('protected'), array()),
         );
+    }
+
+    /**
+     * Test that array properties are not suddenly reference each other (this may happen because of internal
+     * implementation)
+     */
+    public function testPropertiesNotLinked()
+    {
+        $property1 = '_data';
+        $property2 = '_underscoreCache';
+
+        $obj = new Varien_Object();
+        $this->assertAttributeSame(array(), $property1, $obj, "Property {$property1} has wrong default value");
+        $this->assertAttributeSame(array(), $property2, $obj, "Property {$property2} has wrong default value");
+
+        $reflection = new ReflectionClass('Varien_Object');
+        $refProperty1 = $reflection->getProperty($property1);
+        $refProperty1->setAccessible(true);
+        $refProperty1->setValue($obj, array(1, 2, 3));
+
+        $this->assertAttributeSame(array(), $property2, $obj,
+            "Properties {$property2} and {$property1} are linked to each other");
     }
 }
--- varien_object.c
+++ varien_object.c
@@ -1,16 +1,47 @@
 #include "php.h"
 #include "varien_object.h"
 
+// ---Protected property declarations-------
+typedef struct {
+	char *name;
+	uint name_len;
+	zend_uchar type;
+	long default_value;
+} vo_property_declaration_entry;
+
+// Macros for property declarations
+#define VO_DECLARE_PROP_ARRAY(property_name) {#property_name, sizeof(#property_name) - 1, IS_ARRAY, 0}
+#define VO_DECLARE_PROP_BOOL(property_name, value) {#property_name, sizeof(#property_name) - 1, IS_BOOL, value}
+#define VO_DECLARE_PROP_NULL(property_name) {#property_name, sizeof(#property_name) - 1, IS_NULL, 0}
+#define VO_DECLARE_PROP_END {NULL, 0, 0, 0}
+
+// Property declaration
+static const vo_property_declaration_entry vo_property_declarations[] = {
+	VO_DECLARE_PROP_ARRAY(_data),
+	VO_DECLARE_PROP_BOOL(_hasDataChanges, FALSE),
+	VO_DECLARE_PROP_NULL(_origData),
+	VO_DECLARE_PROP_NULL(_idFieldName),
+	VO_DECLARE_PROP_ARRAY(_underscoreCache),
+	VO_DECLARE_PROP_BOOL(_isDeleted, FALSE),
+	VO_DECLARE_PROP_ARRAY(_oldFieldsMap),
+	VO_DECLARE_PROP_ARRAY(_syncFieldsMap),
+	VO_DECLARE_PROP_END
+};
+
+// ---Used variables---
 static zend_class_entry *vo_class;
 static int vo_def_props_num;
 
-// Declarations
-static zend_object_value vo_create_handler(zend_class_entry *type TSRMLS_DC);
+// Forward declarations
+static zend_object_value vo_create_handler(zend_class_entry *class_type TSRMLS_DC);
+static void vo_create_default_array_properties(zend_object_value *obj_value TSRMLS_DC);
 
 // Module initialization. Register Varien_Object class.
 int mage_varien_object_minit(TSRMLS_D)
 {
 	zend_class_entry ce;
+	int i;
+	const vo_property_declaration_entry *prop_declaration;
 
 	//---Class---
 	INIT_CLASS_ENTRY(ce, "Varien_Object", NULL);
@@ -23,7 +54,25 @@ int mage_varien_object_minit(TSRMLS_D)
 	vo_class->create_object = vo_create_handler;
 
 	//---Properties---
-	zend_declare_property_null(vo_class, "_data", sizeof("_data") - 1, ZEND_ACC_PROTECTED TSRMLS_CC);
+	// Note: array properties are initialized to arrays in create_handler
+	for (i = 0; ; i++) {
+		prop_declaration = &vo_property_declarations[i];
+		if (!prop_declaration->name_len) {
+			break;
+		}
+		switch (prop_declaration->type) {
+			case IS_BOOL:
+				zend_declare_property_bool(vo_class, prop_declaration->name, prop_declaration->name_len, prop_declaration->default_value, ZEND_ACC_PROTECTED TSRMLS_CC);
+				break;
+			case IS_NULL:
+			case IS_ARRAY: // Arrays are initially declared as NULLs, but are reinitialized to arrays in the create_handler
+				zend_declare_property_null(vo_class, prop_declaration->name, prop_declaration->name_len, ZEND_ACC_PROTECTED TSRMLS_CC);
+				break;
+			default:
+				php_error_docref(NULL TSRMLS_CC, E_ERROR, "Unknown property declaration type %s:%d", prop_declaration->name, prop_declaration->type);
+				break;
+		}
+	};
 
 	vo_def_props_num = zend_hash_num_elements(&vo_class->default_properties);
 
@@ -31,15 +80,11 @@ int mage_varien_object_minit(TSRMLS_D)
 }
 
 // Custom handler, called on Varien_Object creation. Initializes default properties that must have array type.
-zend_object_value vo_create_handler(zend_class_entry *class_type TSRMLS_DC)
+static zend_object_value vo_create_handler(zend_class_entry *class_type TSRMLS_DC)
 {
 	zend_object *object;
 	zend_object_value retval;
-	zval *obj_zval;
 	zval *tmp;
-
-	zval *array_property;
-	HashTable *ht;
 
 	// Standard initialization
 	retval = zend_objects_new(&object, class_type TSRMLS_CC);
@@ -51,24 +96,47 @@ zend_object_value vo_create_handler(zend_class_entry *class_type TSRMLS_DC)
 	zend_hash_copy(object->properties, &class_type->default_properties, zval_copy_property_ctor(class_type), (void *) &tmp, sizeof(zval *));
 
 	// Update properties that must be arrays by default
-	MAKE_STD_ZVAL(obj_zval);
-	Z_TYPE_P(obj_zval) = IS_OBJECT;
-	Z_OBJVAL_P(obj_zval) = retval;
-
-	ALLOC_HASHTABLE(ht);
-	if (zend_hash_init(ht, 16, NULL, ZVAL_PTR_DTOR, 1) == FAILURE) { // Optimization - pre-allocate buffer for 16 buckets
-		FREE_HASHTABLE(ht);
-		php_error_docref(NULL TSRMLS_CC, E_ERROR, "Unable to init HashTable for object default property");
-	}
-
-	MAKE_STD_ZVAL(array_property);
-	Z_TYPE_P(array_property) = IS_ARRAY;
-	Z_ARRVAL_P(array_property) = ht;
-
-	zend_update_property(class_type, obj_zval, "_data", sizeof("_data") - 1, array_property TSRMLS_CC);
-
-	FREE_ZVAL(obj_zval);
-	zval_ptr_dtor(&array_property); // It has been saved saved as object property, so just decrease refcount
+	vo_create_default_array_properties(&retval TSRMLS_CC);
 
 	return retval;
+}
+
+static void vo_create_default_array_properties(zend_object_value *obj_value TSRMLS_DC)
+{
+	zval *obj_zval;
+
+	int i;
+	const vo_property_declaration_entry *prop_declaration;
+	zval *array_property;
+	HashTable *ht;
+
+	MAKE_STD_ZVAL(obj_zval);
+	Z_TYPE_P(obj_zval) = IS_OBJECT;
+	Z_OBJVAL_P(obj_zval) = *obj_value;
+
+	for (i = 0; ; i++) {
+		prop_declaration = &vo_property_declarations[i];
+		if (!prop_declaration->name_len) {
+			break;
+		}
+		if (prop_declaration->type != IS_ARRAY) {
+			continue;
+		}
+
+		ALLOC_HASHTABLE(ht);
+		if (zend_hash_init(ht, prop_declaration->default_value, NULL, ZVAL_PTR_DTOR, 1) == FAILURE) { // Optimization - pre-allocate buffer for "default_value" buckets
+			FREE_HASHTABLE(ht);
+			php_error_docref(NULL TSRMLS_CC, E_ERROR, "Unable to init HashTable for object default property");
+		}
+
+		MAKE_STD_ZVAL(array_property);
+		Z_TYPE_P(array_property) = IS_ARRAY;
+		Z_ARRVAL_P(array_property) = ht;
+
+		zend_update_property(vo_class, obj_zval, prop_declaration->name, prop_declaration->name_len, array_property TSRMLS_CC);
+
+		zval_ptr_dtor(&array_property); // It has been saved saved as object property, so just decrease refcount
+	}
+
+	FREE_ZVAL(obj_zval);
 }
